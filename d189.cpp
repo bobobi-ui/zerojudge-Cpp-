b450e4e885773a5f38a69b1c8b505eef71b20a94
bobobi-ui <p980124980124@gmail.com>
2025-12-23 19:52:37 +0800

    Add files via upload

---
+++ d189.cpp
@@ -0,0 +1,20 @@
+#include<stdio.h> 
+#include<stdlib.h> 
+
+int main(){
+	
+	int a,i,j,m=0,n=0,t;
+	
+	
+	while(scanf("%d",&a) == 1){
+		
+		a=(a*3)/2;
+		printf("%d\n",a); 
+			
+	}
+	
+	
+	
+	
+	return 0;
+}
