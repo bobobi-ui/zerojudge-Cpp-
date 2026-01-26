1ee25781da3aef5154ee593c9a9353b1a488cedb
bobobi-ui <p980124980124@gmail.com>
2026-01-26 19:21:12 +0800

    Add files via upload

---
+++ c044.cpp
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+	int n;
+	char c;
+	char ch[26];
+	scanf("%d",&n);
+	
+	for(i=0;i<3;i++){
+		while((c=getchar())!=EOF){
+			
+			
+			
+			
+		}	
+	}
+	
+	
+	return 0;
+} 
