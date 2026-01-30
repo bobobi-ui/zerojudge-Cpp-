acf254f2857193cbee801b1824086259579a2937
bobobi-ui <p980124980124@gmail.com>
2026-01-30 17:00:21 +0800

    Update d186.cpp

--- d186.cpp
+++ d186.cpp
@@ -3,22 +3,18 @@
 
 int main(){
 	
-	int i,j,k,a,b;
+	int i,j,a,b;
 	
 	while(scanf("%d %d",&a,&b)==2){
 		j=0;
 		if(a==0 && b==0)
 			break;	
-		for(i=1;a*a<=b;i++){
+		for(i=1;i*i<=b;i++){
 			if(i*i>=a)
 				j++;
 		}
 		printf("%d\n",j);
 	}
-	
-	
-	
-	
-	
 	return 0;
 }
+
