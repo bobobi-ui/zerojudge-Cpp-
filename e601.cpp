9851421604b52bc08f0f69a6d8f7c4d30affa768
bobobi-ui <p980124980124@gmail.com>
2026-02-05 21:40:19 +0800

    Add files via upload

---
+++ e601.cpp
@@ -0,0 +1,72 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int max(int a,int b){
+	if(a>b)
+		return a;
+	else
+		return b;
+}
+
+int min(int a,int b){
+	if(a<b)
+		return a;
+	else
+		return b;
+}
+
+int main(){
+	
+	int k,q,n;
+
+	while(scanf("%d %d %d",&k,&q,&n)==3){
+		int qr,nr,qc,nc,kr,kc;
+		
+		//Illegal state
+		if(k==q){
+			printf("Illegal state\n");
+			continue;
+		}
+		
+		
+		//Illegal move
+		qr=q/8,qc=q%8;
+		nr=n/8,nc=n%8;
+		kr=k/8,kc=k%8;
+
+		if(q==n||n==k){
+			printf("Illegal move\n");
+			continue;
+		}	
+		if(qr!=nr && qc!=nc){
+			printf("Illegal move\n");
+			continue;
+		}	
+		if(qr==kr && k<max(q,n) && k>min(q,n)){
+			printf("Illegal move\n");
+			continue;
+		}
+		if(qc==kc && k<max(q,n) && k>min(q,n)){
+			printf("Illegal move\n");
+			continue;
+		}
+		
+		//Move not allowed
+		
+		if(n==(k-1)||n==(k+1)||n==(k-8)||n==(k+8)){
+			printf("Move not allowed\n");
+			continue;		
+		}
+
+		//Continue & Stop
+		if((n==9 && k==0)||(n==14&&k==7)||(n==49&&k==56)||(n==54&&k==63)){
+			printf("Stop\n");
+			continue;
+		}
+		else{
+			printf("Continue\n");
+			continue;
+		}
+	}
+	return 0;
+} 
